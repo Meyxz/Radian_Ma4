8de063257257780700adcedeb2e7f32666e7f3cc
Meyx <oliver@go-on.se>
2021-09-28 13:37:22 +0200

    Add project files.

---
+++ Radian_Ma4/MyProgram.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Radian_Ma4
+{
+    class MyProgram
+    {
+        public void Run()
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+                DegtoRad(ref isRunning);
+            }
+        }
+
+        void DegtoRad(ref bool isRunning)
+        {
+            Console.Clear();
+            double inputAng = 0;
+            double rad = 0;
+            string inputTxt = string.Empty;
+            bool isDone = false;
+            bool isInputValid = false;
+
+            while (!isInputValid)
+            {
+                Console.WriteLine("Hur många grader?");
+                inputTxt = Console.ReadLine().Trim().Replace(".", ",");
+                isInputValid = double.TryParse(inputTxt, out inputAng);
+                if (!isInputValid)
+                {
+                    Console.Clear();
+                }
+            }
+
+            rad = (Math.PI / 180) * inputAng;
+            rad = Math.Round(rad, 2);
+            Console.WriteLine("{0}° blir {1} radianer", inputAng, rad);
+            Console.WriteLine("\nTryck Y för att testa igen");
+            Console.WriteLine("Tryck N för att avsluta");
+            while (!isDone)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    if (keyInfo.Key == ConsoleKey.N)
+                    {
+                        isRunning = false;
+                        isDone = true;
+                    }
+                    else if (keyInfo.Key == ConsoleKey.Y)
+                    {
+                        isDone = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Radian_Ma4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Radian_Ma4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyProgram myProgram;
+            myProgram = new MyProgram();
+            myProgram.Run();
+        }
+    }
+}
